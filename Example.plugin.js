d04ce99ab4a79c2a58c85c7331c5b1655e833339
Forprix <81227518+Forprix@users.noreply.github.com>
2021-03-23 13:01:21 +0300

    Add files via upload

---
+++ Example.plugin.js
@@ -0,0 +1,33 @@
+//META{"name":"Example","displayName":"Example Plugin"}*//
+
+class Example {
+	//misc
+	unload() { }
+	constructor() { }
+	initialize() { }
+	onMessage() { } 
+	observer(e) { } 
+	onSwitch() { }
+	load() { } 
+
+	//meta
+	getName() { return "Example"; }
+	getDescription() { return "123456789"; }
+	getVersion() { return "1.0"; }
+	getAuthor() { return "Forprix"; }
+
+	//html options for plugin
+	getSettingsPanel() {
+		return "<b>Enter something: </b><input></input>"
+	}
+	//enabling plugin
+	start() {
+        console.log(`${this.getName()} start()`)
+	}
+	//disabling plugin
+	stop() {
+        console.log(`${this.getName()} stop()`)
+	}
+
+}
+
